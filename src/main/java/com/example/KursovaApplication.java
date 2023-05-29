97583daec1af21a49d0cc58b0cd189ba36494c13
d4ng3r228 <dangersof228@gmail.com>
2023-05-29 21:06:48 +0300

    Initial commit

---
+++ src/main/java/com/example/KursovaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursovaApplication.class, args);
+	}
+
+}
